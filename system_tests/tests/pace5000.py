b2822529c1a0b9dce704582fbbad197831cc8ae7
Nikola Roev <nikola.roev@stfc.ac.uk>
2023-06-28 14:55:49 +0100

    Add tests.

--- system_tests/tests/pace5000.py
+++ system_tests/tests/pace5000.py
@@ -1,9 +1,10 @@
 import unittest
+from parameterized import parameterized
 
 from utils.channel_access import ChannelAccess
-from utils.ioc_launcher import get_default_ioc_dir
+from utils.ioc_launcher import get_default_ioc_dir, IOCRegister
 from utils.test_modes import TestModes
-from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
+from utils.testing import get_running_lewis_and_ioc, parameterized_list
 
 
 DEVICE_PREFIX = "PACE5000_01"
@@ -28,7 +29,59 @@ class Pace5000Tests(unittest.TestCase):
     """
     def setUp(self):
         self._lewis, self._ioc = get_running_lewis_and_ioc("Pace5000", DEVICE_PREFIX)
-        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
+        self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX, default_wait_time=0)
+        self._reset_device()
 
-    def test_that_fails(self):
-        self.fail("You haven't implemented any tests!")
+    def _reset_device(self):
+        if IOCRegister.uses_rec_sim:
+            self.ca.set_pv_value(f"SIM:PRESSURE", 0.0)
+            self.ca.set_pv_value(f"SIM:SLEW", 0.0)
+            self.ca.set_pv_value(f"SIM:SLEW:MODE", 0)
+            self.ca.set_pv_value(f"SIM:UNITS", "BAR")
+            self.ca.set_pv_value(f"SIM:STATE", 0)
+            self.ca.set_pv_value(f"SIM:EFFORT", 0)
+            self.ca.set_pv_value(f"SIM:LIMIT:UPPER", 0.0)
+            self.ca.set_pv_value(f"SIM:LIMIT:LOWER", 0.0)
+            self.ca.set_pv_value(f"SIM:ERROR", "0, \"No error\"")
+        else:
+            self._lewis.backdoor_run_function_on_device("reset")
+
+    def _set(self, pv, device_variable, value):
+        if IOCRegister.uses_rec_sim:
+            self.ca.set_pv_value(f"SIM:{pv}", value)
+        else:
+            self._lewis.backdoor_set_on_device(device_variable, value)
+
+    @parameterized.expand(parameterized_list([
+        ("PRESSURE",    "pressure",     0.5),
+        ("EFFORT",      "effort",       12.5),
+        ("LIMIT:UPPER", "limit_upper",  0.7),
+        ("LIMIT:LOWER", "limit_lower",  0.3),
+        ("ERROR",       "error",        "202, \"No query allowed\"")
+    ]))
+    def test_WHEN_read_only_pv_set_THEN_pv_read_correctly(self, _, pv, device_variable, value):
+        self._set(pv, device_variable, value)
+        self.ca.assert_that_pv_is(pv, value)
+    
+    @parameterized.expand(parameterized_list([
+        ("PRESSURE:SP:RBV", "PRESSURE:SP",  0.5),
+        ("SLEW",            "SLEW:SP",      0.3),
+        ("SLEW:MODE",       "SLEW:MODE:SP", "LIN"),
+        ("UNITS",           "UNITS:SP",     "ATM"),
+        ("STATE",           "STATE:SP",     "Control")
+    ]))
+    def test_WHEN_pv_set_THEN_pv_read_correctly(self, _, pv, sp, value):
+        self.ca.set_pv_value(sp, value)
+        self.ca.assert_that_pv_is(pv, value)
+
+    def test_WHEN_stop_issued_THEN_pressure_set_correctly(self):
+        pressure = 12.0
+        self._set("PRESSURE", "pressure", pressure)
+
+        self.ca.assert_that_pv_is("PRESSURE", pressure)
+        self.ca.assert_that_pv_is_not("PRESSURE:SP", pressure)
+        self.ca.assert_that_pv_is_not("PRESSURE:SP:RBV", pressure)
+
+        self.ca.set_pv_value("STOP", 1)
+        self.ca.assert_that_pv_is("PRESSURE:SP", pressure)
+        self.ca.assert_that_pv_is("PRESSURE:SP:RBV", pressure)
